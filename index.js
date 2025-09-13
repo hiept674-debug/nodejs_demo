51c47485f2097ec9cea940c2962356312c96b8d5
cuoiky <thanhhiep2104@gmail.com>
2025-09-13 17:02:24 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function showinfo(name,age){
     console.log(`Name: ${name}, Age ${age}`)
-    console.log("hoang ngu")
+    
 }
-showinfo("Hiep",22)
+
+
+function Age(birth_year){
+    const current_year = new Date().getFullYear();
+    return current_year - birth_year
+}
+
+let name = "Hiep"
+let birth_year = 2005
+let age = Age(birth_year)
+showinfo(name,age)
